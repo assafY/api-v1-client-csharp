c083980ec4c3b7a719b5a6b5ba89d28106dd27e6
Assaf <assafyossifoff@sky.com>
2017-03-22 17:07:15 +0000

    fixed transaction broadcaster whitespaces

--- src/Info.Blockchain.Api/PushTx/TransactionBroadcaster.cs
+++ src/Info.Blockchain.Api/PushTx/TransactionBroadcaster.cs
@@ -4,35 +4,35 @@ using Info.Blockchain.Api.Client;
 
 namespace Info.Blockchain.Api.PushTx
 {
-	/// <summary>
-	/// This class reflects the functionality provided at https://blockchain.info/pushtx.
-	/// It allows users to broadcast hex encoded transactions to the bitcoin network.
-	/// </summary>
-	public class TransactionBroadcaster
-	{
-		private readonly IHttpClient _httpClient;
-		public TransactionBroadcaster()
-		{
-			_httpClient = new BlockchainHttpClient();
-		}
-		public TransactionBroadcaster(IHttpClient httpClient)
-		{
-			_httpClient = httpClient;
-		}
+    /// <summary>
+    /// This class reflects the functionality provided at https://blockchain.info/pushtx.
+    /// It allows users to broadcast hex encoded transactions to the bitcoin network.
+    /// </summary>
+    public class TransactionBroadcaster
+    {
+        private readonly IHttpClient _httpClient;
+        public TransactionBroadcaster()
+        {
+            _httpClient = new BlockchainHttpClient();
+        }
+        public TransactionBroadcaster(IHttpClient httpClient)
+        {
+            _httpClient = httpClient;
+}
 
-		/// <summary>
-		/// Pushes a hex encoded transaction to the network.
-		/// </summary>
-		/// <param name="transactionString">Hex encoded transaction</param>
-		/// <exception cref="ServerApiException">If the server returns an error</exception>
-		public async Task PushTransactionAsync(string transactionString)
-		{
-			if (string.IsNullOrWhiteSpace(transactionString))
-			{
-				throw new ArgumentNullException(nameof(transactionString));
-			}
+        /// <summary>
+        /// Pushes a hex encoded transaction to the network.
+        /// </summary>
+        /// <param name="transactionString">Hex encoded transaction</param>
+        /// <exception cref="ServerApiException">If the server returns an error</exception>
+        public async Task PushTransactionAsync(string transactionString)
+        {
+            if (string.IsNullOrWhiteSpace(transactionString))
+            {
+                throw new ArgumentNullException(nameof(transactionString));
+            }
 
             await _httpClient.PostAsync<string, object>("pushtx", transactionString, multiPartContent: true);
-		}
-	}
+        }
+    }
 }
